c66d56e4ba92bfd386ea961f0a6e7e683ec36ec7
Cole Chamberlain <cole.chamberlain@gmail.com>
2015-07-08 01:17:04 -0700

    initx2

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-"use strict";
-
-var platform = process.env.platform
+var platform = process.env.platform;
 
 exports.platform = platform;
 exports.platformv = getVariant();
@@ -9,18 +7,18 @@ function getVariant() {
   if(platform !== "win32") {
     return platform;
   }
-  var path = process.env.PATH.toLowerCase()
-    , order: [
-      { "msys2": path.indexOf("msys") },
-      { "mingw": path.indexOf("mingw") },
-      { "cygwin": path.indexOf("cygwin") }
-    ]
-  };
+
+  var path = process.env.PATH.toLowerCase();
+  var order = [
+    { "msys2": path.indexOf("msys") },
+    { "mingw": path.indexOf("mingw") },
+    { "cygwin": path.indexOf("cygwin") }
+  ];
+
   if(order.msys === -1 && order.mingw === -1 && order.cygwin === -1) {
     return "win32";
-  } else {
-    return order.sort(function(a, b) {
-      return a[1] - b[1];
-    })[0];
   }
+  return Object.keys(order.sort(function(a, b) {
+    return a[1] - b[1];
+  })[0])[0];
 }
